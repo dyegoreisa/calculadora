1d19172ff59fd7b8fd981357f88cc0c3ab6f0851
Dyego Reis de Azevedo <dyegoreisa@gmail.com>
2016-06-19 20:20:42 -0300

    Implementação da entrada pelo teclado no terminal

--- CalcTerminal/src/br/com/biavan/calcterminal/CalcTerminal.java
+++ CalcTerminal/src/br/com/biavan/calcterminal/CalcTerminal.java
@@ -1,29 +1,23 @@
 package br.com.biavan.calcterminal;
 
+import java.util.Scanner;
+
 public class CalcTerminal {
 
 	public static void main(String[] args) {
 
-		Number num1 = new Number(Double.valueOf(15));
-		Number num2 = new Number(Double.valueOf(32.8));
-		Number num3 = new Number(Double.valueOf(6));
-		Number num4 = new Number(Double.valueOf(24));
-		Number num5 = new Number(Double.valueOf(4));
-		Number num6 = new Number(Double.valueOf(3));
-		
-		
-		System.out.println(num1.add(num3));
-		System.out.println(num4.subtract(num3));
-		System.out.println(num3.multiplies(num5));
-		
-		System.out.print(num6.divides(num3));
-		System.out.println(" Resta: " + num6.getRemainder());
-		
-		System.out.print(num2.divides(num3));
-		System.out.println(" Resta: " + num2.getRemainder());
-		
-		System.out.print(num3.divides(num6));
-		System.out.println(" Resta: " + num3.getRemainder());
+	    Scanner scanner = new Scanner(System.in);
+
+	    System.out.println("--<<< < < < [ Calculadora ] > > > >>>--");
+	    
+	    String parm = "";
+	    StringBuffer sb = new StringBuffer();
+	    do {
+	    	parm = scanner.next();
+	    	sb.append(parm);
+	    }while(!parm.contains("="));
+	    
+	    System.out.println(sb.toString());
 
 	}
 
