22557197a7ab301290fe4961dac03997ba2ec5e3
Dyego Reis de Azevedo <dyegoreisa@gmail.com>
2016-06-19 14:59:24 -0300

    Inclusão do exemplo de divisao

--- CalcTerminal/src/br/com/biavan/calcterminal/CalcTerminal.java
+++ CalcTerminal/src/br/com/biavan/calcterminal/CalcTerminal.java
@@ -9,9 +9,19 @@ public class CalcTerminal {
 		Number num3 = new Number(Double.valueOf(6));
 		Number num4 = new Number(Double.valueOf(24));
 		Number num5 = new Number(Double.valueOf(4));
+		Number num6 = new Number(Double.valueOf(3));
 		
 		
 		System.out.println(num1.add(num3));
+		
+		System.out.print(num6.divides(num3));
+		System.out.println(" Resta: " + num6.getRemainder());
+		
+		System.out.print(num2.divides(num3));
+		System.out.println(" Resta: " + num2.getRemainder());
+		
+		System.out.print(num3.divides(num6));
+		System.out.println(" Resta: " + num3.getRemainder());
 
 	}
 
